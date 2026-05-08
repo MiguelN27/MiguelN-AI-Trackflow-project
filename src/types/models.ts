b1f9331effef889b38ae3188a594718a89bea0b0
Miguel Narvaez <83840366+MiguelN27@users.noreply.github.com>
2026-05-08 10:50:11 +0000

    1st model interfce

---
+++ src/types/models.ts
@@ -0,0 +1,15 @@
+// interfaces and types right here, here we'll go all the interfaces for the main entities of the company defined in context.md
+
+/* INterface model:
+
+interface Entity {
+    id: string;
+    department: string;
+    createdAt: Date;
+    updatedAt: Date;
+    location: string;
+    manager: string;
+    team: number;
+}
+
+*/ 
